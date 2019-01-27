707dfe1e8fbb4e5f29540c615ae73b601dc354dd
Jennifer Chen <jenn.chen@live.ca>
2019-01-26 18:54:57 -0800

    Removed files, left with skeleton app

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+// Import all your components to the App.js 
+// import { QuizBox } from "./QuizBox";
+// import { AnswerRevealBox } from "./AnswerRevealBox"
+
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export { App };
