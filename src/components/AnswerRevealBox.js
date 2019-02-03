c4f6c8d3e9b37304a6cd99b22da7499d68d67bdb
Jennifer Chen <jenn.chen@live.ca>
2019-02-02 21:42:29 -0800

    Comma join multiple correct romaji spellings

--- src/components/AnswerRevealBox.js
+++ src/components/AnswerRevealBox.js
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 
+// function test(arg){
+//   if (Array.isArray(arg) === true){
+//     //display with comma in between; append to string
+//     return arg.join(", ");
+//   } else {
+//     return "Argument not an array";
+//   }
+// }
+
 class AnswerRevealBox extends Component {
   render(){
     return(
       <div className="AnswerRevealBox">
         <h6 style={{ color: "purple", fontSize: "22px"}}>
           {/* Adjust to allow for a list of romaji spellings */}
-          From AnswerRevealBox: {this.props.prevChar} - {this.props.prevCharRomaji}
+          From AnswerRevealBox: {this.props.prevChar}
+          {Array.isArray(this.props.prevCharRomaji) ? (
+            ` - ${this.props.prevCharRomaji.join(", ")}`
+          ) : (
+            this.props.prevCharRomaji
+          )}
         </h6>
       </div>
     )
